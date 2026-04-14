4eda3943d359276416dfc3c11a7f928801f4fd10
magda <47281888+arquesm@users.noreply.github.com>
2026-04-14 14:59:26 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -6,50 +6,26 @@ const SUPABASE_URL = "https://hrvufltqcnyfzvlnbmzi.supabase.co"; // replace with
 const SUPABASE_KEY = "sb_publishable_awlsvjfkZ-usQ26F7fHH5w_49iZXcal"; // replace with your anon key
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY); 
 
-async function getPosts() {
-  const { data, error } = await sb.from("posts").select("*");
+let posts = [];
 
-  const scoreboard = document.getElementById("scoreboard");
-  scoreboard.innerHTML = "<h2>Scoreboard</h2>";
-  posts.forEach((row) => {
-    const div = document.createElement("div");
-    div.textContent = `${row.name}: ${row.score}`;
-    scoreboard.appendChild(div);
-  });
- posts = data; 
+async function getPosts() {
+  const { data, error } = await sb
+    .from('posts')
+    .select('*');
+
   if (error) {
-    console.error("ERROR:", error);
-  } else {
-    console.log("DATA:", data);
+    console.error(error);
+    return;
   }
+
+  posts = data;
+
+  console.log("Posts loaded:", posts);
 }
 
 async function setup() {
-  background(100);
   createCanvas(400, 400);
-  await getPosts(); // wait for data
-
-  console.log("Posts loaded:", 'posts');
-  
-  const { data: posts, error: fetchError } = await sb.from("posts").select("*");
-
-  if (fetchError) console.error("Fetch error:", fetchError);
-  else {
-    if (posts.length === 0) {
-      await sb.from("posts").insert([
-        {
-          tema: "Alilgtbice",
-          col·lectiu: "d8m",
-          content: "totes lliures",
-          url_img: "http://www.dones8m.es",
-          any: 2026,
-          lloc: "Alcoi",
-        },
-      ]);
-      console.log("Inserted Alice");
-    }
-    getPosts(posts);
-  }
+  await getPosts();
 }
 
 function draw() {

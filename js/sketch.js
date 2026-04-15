753f658a16ffe521dbf79b6d7bb7a8a53cd380ac
magda <47281888+arquesm@users.noreply.github.com>
2026-04-15 16:42:50 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -23,11 +23,11 @@ function renderPosts(posts) {
 
     card.innerHTML = `
       <h2>${post.topic || "no"}</h2>
-      <p><strong>Col·lectiu:</strong> ${post.colectiu || "no"}</p>
-      <p><strong>Content:</strong>${post.content || ""}</p>
-      <p><strong>Any:</strong> ${post.any || "no"}</p>
-      <p><strong>Lloc:</strong> ${post.lloc || "no"}</p>
-      <img src="${post.url_img || "no"}" />
+      <p><strong>Col·lectiu:</strong> ${post.colectiu || " - "}</p>
+      <p><strong>Content:</strong>${post.content || " - "}</p>
+      <p><strong>Any:</strong> ${post.any || " - "}</p>
+      <p><strong>Lloc:</strong> ${post.lloc || " - "}</p>
+      <img src="${post.url_img || " - "}" />
     `;
     container.appendChild(card);
   });

4576caa02d0bc88554d86ee7bccb731089b05edb
magda <47281888+arquesm@users.noreply.github.com>
2026-04-15 12:33:26 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -63,7 +63,7 @@ function draw() {
     text(content, 20, y - 20);
     text(any, 20, y);
     text(lloc, 20, y + 20);
-    
+    text(img_irl,20, y+ 40);
     /*for (let j = 1; j < columns.length + 1; j++) {
       const fields = [topic, colectiu, content,  any, lloc, url_img];
       const colWidth = 100; // adjust to your needs

d391e9770cdc3b242cf9bea5c6800b72716db1c3
magda <47281888+arquesm@users.noreply.github.com>
2026-04-15 16:28:52 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@ const SUPABASE_URL = "https://hrvufltqcnyfzvlnbmzi.supabase.co"; // replace with
 const SUPABASE_KEY = "sb_publishable_awlsvjfkZ-usQ26F7fHH5w_49iZXcal"; // replace with your anon key
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+
 async function loadPosts() {
   const { data, error } = await sb.from("posts").select("*");
   if (error) {
@@ -22,8 +23,8 @@ function renderPosts(posts) {
 
     card.innerHTML = `
       <h2>${post.topic || "no"}</h2>
-      <p><strong>Col·lectiu:</strong> ${post.colectiu || "no"}</p>
-      <p>${post.content || "no"}</p>
+      <p><strong>Colectiu:</strong> ${post.colectiu || "no"}</p>
+      <p>${post.content || ""}</p>
       <p><strong>Any:</strong> ${post.any || "no"}</p>
       <p><strong>Lloc:</strong> ${post.lloc || "no"}</p>
       <img src="${post.url_img || "no"}" />
